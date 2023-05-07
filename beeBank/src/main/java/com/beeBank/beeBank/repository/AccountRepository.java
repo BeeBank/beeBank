86e896f5d5ed11b77feefe009cfb5d2520792802
nora <sxj042@gmail.com>
2023-05-07 14:04:32 -0400

    changing

--- beeBank/src/main/java/com/beeBank/beeBank/repository/AccountRepository.java
+++ beeBank/src/main/java/com/beeBank/beeBank/repository/AccountRepository.java
@@ -3,6 +3,9 @@ package com.beeBank.beeBank.repository;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -17,6 +20,13 @@ List<Account> getUserAccountsById(@Param("user_id")int user_id);
 
 @Query(value = "SELECT sum(balance) FROM accounts WHERE user_id = :user_id", nativeQuery = true)
 BigDecimal getTotalBalance(@Param("user_id") int user_id);
+
+@Modifying
+@Query(value = "INSERT INTO accounts(user_id, account_number, account_name, account_type) VALUES" +
+"(:user_id, :account_number, :account_name, :account_type )", nativeQuery = true)
+@Transactional
+void createBankAccount(@Param("user_id") int user_id, @Param("account_number") String account_number, @Param("account_name") String account_name, @Param("account_type") String account_type);
+
 
 
 
