4605657030cee03748a7c913b67121c0dd31d756
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-05-08 16:01:47 -0400

    fix

--- beeBank/src/main/java/com/beeBank/beeBank/controllers/indexController.java
+++ beeBank/src/main/java/com/beeBank/beeBank/controllers/indexController.java
@@ -14,5 +14,26 @@ public class indexController {
         System.out.println("In index Controller");
         return getIndexPage;
     }
+    // @GetMapping("/login")
+    // public ModelAndView getLogin(){
+    //     ModelAndView getIndexPage = new ModelAndView("login");
+    //     System.out.println("In Login Page Controller");
+    //     getIndexPage.addObject("PageTitle", "Login");
+    //     return getLoginPage;
+    // }
+    // @GetMapping("/register")
+    // public ModelAndView getRegister(){
+    //     ModelAndView getIndexPage = new ModelAndView("register");
+    //     System.out.println("In Register Page Controller");
+    //     getIndexPage.addObject("PageTitle", "Register");
+    //     return getRegisterPage;
+    // }
+    // @GetMapping("/error")
+    // public ModelAndView getError(){
+    //     ModelAndView getIndexPage = new ModelAndView("error");
+    //     System.out.println("In Register Page Controller");
+    //     getIndexPage.addObject("PageTitle", "Errors");
+    //     return getErrorPage;
+    // }
     
 }
