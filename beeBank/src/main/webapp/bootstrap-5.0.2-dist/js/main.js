dd0d602978b7dc42ccf26b88ee32219c425fd696
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-05-09 16:42:08 -0400

    finished left OffCanvas forms

--- beeBank/src/main/webapp/bootstrap-5.0.2-dist/js/main.js
+++ beeBank/src/main/webapp/bootstrap-5.0.2-dist/js/main.js
@@ -7,19 +7,36 @@ const transferCard = document.querySelector(".transfer-card");
 const depositCard = document.querySelector(".deposit-card");
 const withdrawalCard = document.querySelector(".withdrawal-card");
 
-
-transactType.addEventListener("change", function(){
+//Check for Transaction Type Event Listener
+transactType.addEventListener("change", () => {
     //Check for transaction type and display form:
     switch(transactType.value){
         case "payment":
             paymentCard.style.display = "block";
             paymentCard.nextElementSibling.style.display = "none";
+            depositCard.style.display = "none";
+            withdrawalCard.style.display = "none";
         break;
 
         case "transfer":
             transferCard.previousElementSibling.style.display = "none";
             transferCard.style.display = "block"
-            // transferCard.nextElementSibling.style.display = "none";
+            transferCard.nextElementSibling.style.display = "none";
+            withdrawalCard.style.display = "none";
+        break;
+
+        case "deposit":
+            depositCard.previousElementSibling.style.display = "none";
+            depositCard.style.display = "block"
+            depositCard.nextElementSibling.style.display = "none";
+            paymentCard.style.display = "none";
+        break;
+
+        case "withdrawal":
+            withdrawalCard.previousElementSibling.style.display = "none";
+            withdrawalCard.style.display = "block"
+            paymentCard.style.display = "none";
+            transferCard.style.display = "none"
         break;
     }
 })
